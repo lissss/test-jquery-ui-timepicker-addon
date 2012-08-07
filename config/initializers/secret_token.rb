079a73b52d460d22624aa1bb07e88232acf5aa69
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2012-08-07 17:28:42 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestJqueryUiTimepickerAddon::Application.config.secret_token = 'fca228c137a43ac850ae09d13e5ee260bdea9c466fe5660ddb21045025c6256733dc4f40cafd3b5f47262b5a363245e29c668cb5e67495b698d637aa80ba073c'
